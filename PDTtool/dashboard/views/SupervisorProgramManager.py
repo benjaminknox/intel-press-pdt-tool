875992d3e4f587976627dfe5c7dae7a5415b7ddb
Benjamin Knox <ben.knox@cummings-inc.com>
2013-05-01 16:46:47 -0700

    Update the dashboard
    Change-Id: I4f6e7de975d7bbf78bc8abdffe0be95a124999a4

--- PDTtool/dashboard/views/SupervisorProgramManager.py
+++ PDTtool/dashboard/views/SupervisorProgramManager.py
@@ -2,14 +2,66 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from dashboard.forms import AddDocumentForm
 
+
+"""
+" The Document Management Author Views.
+"		
+"		TODO: Getting the file security knowledge.
+"""
+########
+# adddocument is a form that allows an Author to add
+#		a document.
+#	-The view file is adddocument.html
+########
+@login_required
 def adddocument(request):
 
+	#Load the object resources
 	context = {
-		'title': 'Add Document',
-		'AddDocumentForm': AddDocumentForm,
+		'title': 'Add a Document',
+		'AddDocumentForm': AddDocumentForm(),
 	}
 
-	#Render the view and return it.
+	#Check if a file has been posted
+	if request.method == 'POST':
+
+		#Get the document post data
+		docName = request.POST['name']
+		docDesc = request.POST['description']
+		docUser = request.user
+		#Create a new Document and save it.
+		doc = Document(name=docName, description=docDesc,user=docUser)
+		doc.save()
+
+		#Get the files
+		files = request.FILES
+
+		for name,f in files.iteritems():
+
+			#Single file upload
+			#Get the file
+			fileName = f.name
+			location = '/home/programmer/upload_dir/'+f.name
+			fileSize = f.size
+
+			#Load a new uploaded file and save it.
+			uploadedfile = File(location = location,
+							    filename=fileName,
+							    size=fileSize,
+							    documentid=doc)
+			uploadedfile.save()
+
+			#Save the file on to the directory.
+			handle_uploaded_file(f,location)
+
+			doc.file.add(uploadedfile)			
+
+		#Return the view
+		return render(request,
+					  'dashboard/SupervisorProgramManager/adddocumentaction.html',
+					  context)
+
+	#Return the view
 	return render(request,
-		  		  'dashboard/SupervisorProgramManager/adddocument.html',
+				  'dashboard/SupervisorProgramManager/adddocument.html',
 				  context)
