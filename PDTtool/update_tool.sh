950accccece43fb0028c0d60a304ef352a72f34a
Benjamin Knox <ben.knox@cummings-inc.com>
2013-05-28 09:05:43 -0700

    Update the update_tool to current settings
    Change-Id: Ice854ca9a4f82d715fe151521e8bc7acb409ff8a

--- PDTtool/update_tool.sh
+++ PDTtool/update_tool.sh
@@ -1,8 +1,12 @@
 #! /bin/bash
 #./manage.py migrate
 
+./manage.py syncdb
+
 ./manage.py schemamigration topic_management meeting_management --auto
 
 ./manage.py migrate
+
+echo "Pulling from git repo:"
 
 git pull origin feature/schedule_centric_design
