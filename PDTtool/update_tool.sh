1b0de00fc3b60b2f4bbf6fd9a4bc002c57db7e8a
Benjamin Knox <ben.knox@cummings-inc.com>
2013-05-28 09:16:15 -0700

    Move git repo pull to top of script.
    Change-Id: I64c0ca105f913cb3df5da0efbd30029d428db458

--- PDTtool/update_tool.sh
+++ PDTtool/update_tool.sh
@@ -1,12 +1,14 @@
 #! /bin/bash
 #./manage.py migrate
 
+echo "Pulling from git repo:"
+
+git pull origin feature/schedule_centric_design
+
+echo "Syncing DB schema:"
+
 ./manage.py syncdb
 
 ./manage.py schemamigration topic_management meeting_management --auto
 
 ./manage.py migrate
-
-echo "Pulling from git repo:"
-
-git pull origin feature/schedule_centric_design
