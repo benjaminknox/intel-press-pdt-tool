9b0e90a912454ca050ab92610482be49e33c6e76
Benjamin Knox <ben.knox@cummings-inc.com>
2013-05-30 13:25:16 -0700

    Update the installer.

--- PDTtool/update_tool.sh
+++ PDTtool/update_tool.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
 #./manage.py migrate
+echo "Installing Needed Software"
+pip install django==1.5.1
+pip install south
+pip install django_extensions
+pip install psycopg2
+pip install python-dateutil
 
 echo "Pulling from git repo:"
 
@@ -17,3 +23,13 @@ echo "Syncing DB schema:"
 #Do it a second time for relationships.
 ./manage.py migrate meeting_management
 ./manage.py migrate topic_management
+
+echo "Creating Directories"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/"
+cd $DIR
+
+mkdir uploaded_topics
+
+mkdir uploaded_topics/approved uploaded_topics/deleted uploaded_topics/uploads
+
+chmod 777 uploaded_topics uploaded_topics/approved uploaded_topics/deleted uploaded_topics/uploads
